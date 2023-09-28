5abecc6579b900b36c4bf9bb882b0e903792e606
kykazabra <kykazabra@gmail.com>
2023-09-28 10:20:58 +0300

    Чуть чуть доковырял

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ import nltk
 class Belinskiy(object):
     def __init__(self, text):
         self.text = text
+        self.morph = pymorphy2.MorphAnalyzer()
         self.tokens = self.to_tokens(self.text)
         self.sentences = self.to_sent(self.text)
-        self.morph = pymorphy2.MorphAnalyzer()
+        self.words = self.parse_words(self.tokens)
+        #nltk.download('words')
+        self.body()
 
     @staticmethod
     def to_tokens(text):
@@ -18,6 +21,14 @@ class Belinskiy(object):
     def to_sent(text):
         return nltk.sent_tokenize(text)
 
+    @staticmethod
+    def parse_words(tokens):
+        words = []
+        for token in tokens:
+            if token not in string.punctuation:
+                words.append(token)
+
+        return words
     @staticmethod
     def find_abbr(text):
         abbr = r'([A-ZА-Я]{2,})'
@@ -34,25 +45,34 @@ class Belinskiy(object):
     def is_plur_noun(self, word):
         form = self.morph.parse(word)[0]
 
-        if 'plur' in form.tag and 'NOUN' in form.tag:
-            return True
-        else:
-            return False
+        return 'plur' in form.tag and 'NOUN' in form.tag
 
     def find_plural_nouns(self, tokens):
         plural_nouns = []
-        for token in self.tokens:
+        for token in self.words:
             if self.is_plur_noun(token):
                 plural_nouns.append(token)
 
         return plural_nouns
 
-    def find_english(self, token):
-        pass
+    @staticmethod
+    def is_english(word):
+        return word in nltk.corpus.words.words()
+
+    def find_english(self, words):
+        eng_words = []
+        for word in words:
+            if self.is_english(word):
+                eng_words.append(word)
+
+        return eng_words
 
     def has_simple_synonym(self, token):
         pass
 
-    def find_digits(self):
-        pass
+    def find_digits(self, text):
+        digit = r'\d+'
 
+        return list(map(int, re.findall(digit, text)))
+    def body(self):
+        print(self.find_digits(self.text))
---
+++ test.py
@@ -0,0 +1,7 @@
+from main import Belinskiy
+
+b = Belinskiy('''Можно и без словаря, через какой-нибудь лексич24еский анализатор, но там не со всеми словами всё будет 
+хорошо, с англицизмами и слэнгом будут проблемы, ну и порог надо подбирать:23
+In the future, I might want to c3456heck if the singular form of a word is in the dictionary (e.2g., properties -> property 
+-> english word). How would I achieve that?
+''')
